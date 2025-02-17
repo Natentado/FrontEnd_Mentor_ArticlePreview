71b1b842c595db224cc6a12894e880b871835cf4
Nathan Molina <itsjustnatemolina@gmail.com>
2025-02-17 07:10:42 -0300

    article preview component layout

---
+++ script.js
@@ -0,0 +1,14 @@
+const shareBtn = document.getElementById("shareBtn");
+const internalShareBtn = document.getElementsByClassName("__internal")[0];
+
+shareBtn.onclick = (el, e) => {
+    const hiddelElement = document.getElementById("hiddenContainer");
+    hiddelElement.classList.remove("closed");
+    hiddelElement.classList.toggle("active");
+}
+
+internalShareBtn.onclick = (el, e) => {
+    const hiddelElement = document.getElementById("hiddenContainer");
+    hiddelElement.classList.toggle("active");
+    hiddelElement.classList.add("closed");
+}
